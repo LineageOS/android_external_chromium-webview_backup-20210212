c186aa47e3361ad2e5babf3865458b8202b982a4
Tim Schumacher <timschumi@gmx.de>
2018-06-26 06:31:53 +0200

    Updated webviews to Chromium 67.0.3396.87
    - Let mips die in peace
    
    Squashed commit of the following:
    
    commit 5b4ec2be6904ec11cad9b0fff637a5510c4ef740
    Author: Luca Stefani <luca.stefani.ge1@gmail.com>
    Date:   Thu Jun 14 11:42:14 2018 +0200
    
        Update README and patches for Chromium 67.0.3396.87
    
        Original-Change-Id: Ic8aa4ce54c73fcf952bdd6b1c8ce3d0f25290c6e
    
    commit 94534378b89241ff315098d84ca7592ad5425df3
    Author: Luca Stefani <luca.stefani.ge1@gmail.com>
    Date:   Thu Jun 14 11:43:01 2018 +0200
    
        Update arm/arm64 webviews Chromium 67.0.3396.87
    
        Original-Change-Id: I84709dbb53c488fd63f8a8a612a6fd3f36a199c4
    
    commit ba9debb535a7c75b8984846de64895c48509a915
    Author: Luca Stefani <luca.stefani.ge1@gmail.com>
    Date:   Thu Jun 14 17:56:01 2018 +0200
    
        Update x86/x86_64 webviews Chromium 67.0.3396.87
    
        Original-Change-Id: Id41342c40e486ba99121d461f9ac60620c12cf00
    
    Change-Id: I48214395d3acc7b6432a82e5f4a2bf36e93127f6

--- Android.mk
+++ Android.mk
@@ -28,13 +28,12 @@ LOCAL_REQUIRED_MODULES := \
         libwebviewchromium_loader \
         libwebviewchromium_plat_support
 
-LOCAL_MODULE_TARGET_ARCH := arm arm64 mips x86 x86_64
+LOCAL_MODULE_TARGET_ARCH := arm arm64 x86 x86_64
 my_src_arch := $(call get-prebuilt-src-arch,$(LOCAL_MODULE_TARGET_ARCH))
 LOCAL_SRC_FILES := prebuilt/$(my_src_arch)/webview.apk
 
 LOCAL_PREBUILT_JNI_LIBS_arm := @lib/armeabi-v7a/libwebviewchromium.so
 LOCAL_PREBUILT_JNI_LIBS_arm64 := @lib/arm64-v8a/libwebviewchromium.so
-LOCAL_PREBUILT_JNI_LIBS_mips := @lib/mips/libwebviewchromium.so
 LOCAL_PREBUILT_JNI_LIBS_x86 := @lib/x86/libwebviewchromium.so
 LOCAL_PREBUILT_JNI_LIBS_x86_64 := @lib/x86_64/libwebviewchromium.so
 
